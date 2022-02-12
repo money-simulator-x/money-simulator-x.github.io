f9e0a18767a0a0ba13a0fae8bdfc923aca672d2d
money-simulator-x <cryptify.12@gmail.com>
2022-02-12 20:29:52 +0200

    Implement getting money, filling bag, and power/capacity upgrades
    this code is really janky but if it works, it works

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log("hi!")
+var bag = 1.00;
+var cash = 0.00;
+var power = 1;
+var capacity = 1;
+console.log("js works");
+function money() {
+  console.log(bag);
+  console.log(cash);
+  if (bag > 0) {
+    if (power / 100 > bag) {
+      cash += bag;
+      bag = 0;
+    } else {
+      cash += (0.01 * power);
+      bag -= (0.01 * power);
+    }
+    if (bag < 0.01 && capacity === 1) {
+      cash += (0.01 * power);
+      bag = 0;
+      document.getElementById("bag").innerHTML = "0";
+    } else {
+      document.getElementById("bag").innerHTML = bag.toFixed(2);
+    }
+    document.getElementById("money").innerHTML = cash.toFixed(2);
+  }
+}
+function fillBag() {
+  bag = capacity;
+  document.getElementById("bag").innerHTML = bag.toFixed(2);
+}
+function power2x() {
+  power *= 2;
+  document.getElementById("power").innerHTML = power;
+}
+function bag2x() {
+  bag += capacity;
+  capacity *= 2;
+  document.getElementById("capacity").innerHTML = capacity;
+  document.getElementById("bag").innerHTML = bag.toFixed(2);
+}
